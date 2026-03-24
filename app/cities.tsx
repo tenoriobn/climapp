f0419a0c824ae47e2d12a76eeedddb32b3b11b0a
Bruno Tenorio <tenoriobn@gmail.com>
2026-03-24 12:44:13 -0300

    feat: :sparkles: 02/07 - Adicionando dados mockados em tela

--- app/cities.tsx
+++ app/cities.tsx
@@ -1,7 +1,40 @@
-import { View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { StyleSheet, Text, View } from "react-native";
+import citiesData from "../data/cities.json";
 
 const Cities = () => {
-  return <View></View>;
+  console.log(citiesData);
+
+  return (
+    <LinearGradient colors={["#00457D", "#05051F"]} style={style.container}>
+      {citiesData.map((city, index) => (
+        <View style={style.listItem} key={index}>
+          <Text style={style.cityName}>{city.city}</Text>
+        </View>
+      ))}
+    </LinearGradient>
+  );
 };
 
 export default Cities;
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+    gap: 16,
+  },
+  listItem: {
+    height: 63,
+    width: "100%",
+    backgroundColor: "rgba(255,255,255, 0.15)",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 16,
+  },
+  cityName: {
+    color: "#fff",
+    fontSize: 16,
+    fontFamily: "Montserrat_500Medium",
+  },
+});
