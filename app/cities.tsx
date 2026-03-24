209109af4616b56bfc6c61ff31e3f389e9640bd1
Bruno Tenorio <tenoriobn@gmail.com>
2026-03-24 13:01:14 -0300

    feat: :sparkles: Adicionando ScrollView para exibir os dados em tela

--- app/cities.tsx
+++ app/cities.tsx
@@ -1,5 +1,5 @@
 import { LinearGradient } from "expo-linear-gradient";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
 import citiesData from "../data/cities.json";
 
 const Cities = () => {
@@ -7,11 +7,20 @@ const Cities = () => {
 
   return (
     <LinearGradient colors={["#00457D", "#05051F"]} style={style.container}>
-      {citiesData.map((city, index) => (
-        <View style={style.listItem} key={index}>
-          <Text style={style.cityName}>{city.city}</Text>
+      <ScrollView>
+        <View style={style.scrollList}>
+          {citiesData.map((city, index) => (
+            <View style={style.listItem} key={index}>
+              <Image
+                source={require("../assets/images/clouds.png")}
+                style={style.cityImage}
+              />
+              <Text style={style.cityName}>{city.city}</Text>
+              <Text style={style.cityTemp}>{city.temp}º</Text>
+            </View>
+          ))}
         </View>
-      ))}
+      </ScrollView>
     </LinearGradient>
   );
 };
@@ -22,6 +31,8 @@ const style = StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: 16,
+  },
+  scrollList: {
     gap: 16,
   },
   listItem: {
@@ -29,12 +40,23 @@ const style = StyleSheet.create({
     width: "100%",
     backgroundColor: "rgba(255,255,255, 0.15)",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "space-between",
     borderRadius: 16,
+    flexDirection: "row",
+    paddingHorizontal: 16,
   },
   cityName: {
     color: "#fff",
     fontSize: 16,
     fontFamily: "Montserrat_500Medium",
+  },
+  cityTemp: {
+    color: "#fff",
+    fontSize: 24,
+    fontFamily: "Montserrat_700Bold",
+  },
+  cityImage: {
+    width: 27,
+    height: 24,
   },
 });
